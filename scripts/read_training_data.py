29a131ecdb1931d0e02832387a79425ed9d29c70
Bradley Collicott <bcollico@stanford.edu>
2022-02-21 19:43:49 -0800

    Updated RVO2 script to output additional info. Updated parser to read new data format.

--- scripts/read_training_data.py
+++ scripts/read_training_data.py
@@ -5,7 +5,7 @@ import os
 # USE IF RUNNING SCRIPT AS STANDALONE
 # cd = os.getcwd()
 # head_dir = cd.split(os.path.sep)[-1]
-#
+# #
 # if head_dir == "aa277_project":
 #     filename = os.path.join("data","training_data.csv")
 # elif head_dir == "scripts":
@@ -21,8 +21,11 @@ class TrainingData():
         self.n_agents   = param_dict["n_agents"]
         self.dt         = param_dict["dt"]
 
+        self.traj = []
+
         # preallocate for data from each episode
-        self.traj       = [OrcaTraj(self.n_agents)]*self.n_episodes
+        for _ in range(self.n_episodes):
+            self.traj.append(OrcaTraj(self.n_agents))
 
 
 class OrcaTraj():
@@ -30,14 +33,31 @@ class OrcaTraj():
 
     def __init__(self, n_agents):
 
-        # access data e.g. self.Px[agent][time]
-        self.Px = [np.array([])]*n_agents # x position for each agent
-        self.Py = [np.array([])]*n_agents # y position for each agent
-        self.Vx = [np.array([])]*n_agents # x velocity for each agent
-        self.Vy = [np.array([])]*n_agents # y velocity for each agent
+        # access vector data e.g. self.Px[agent][time]
+        self.Px = [] # x position for each agent
+        self.Py = [] # y position for each agent
+        self.Vx = [] # x velocity for each agent
+        self.Vy = [] # y velocity for each agent
 
-        # access data eg self.X[agent][time,state_index]
-        self.X = [np.array([])]*n_agents # full state of each robot
+        # access vector data e.g. self.X[agent][time,state_index]
+        self.X  = [] # full state of each robot
+        self.Pg = []  # Goal state for each robot
+
+        # access scalar data e.g. self.R[agent]
+        self.R    = np.zeros(n_agents)
+        self.Vmax = np.zeros(n_agents)
+
+        for _ in range(n_agents):
+            # access vector data e.g. self.Px[agent][time]
+            self.Px.append(np.array([])) # x position for each agent
+            self.Py.append(np.array([])) # y position for each agent
+            self.Vx.append(np.array([])) # x velocity for each agent
+            self.Vy.append(np.array([])) # y velocity for each agent
+
+            # access vector data e.g. self.X[agent][time,state_index]
+            self.X.append(np.array([])) # full state of each robot
+            self.Pg.append(np.zeros(2)) # Goal state for each robot
+
 
 def read_training_data(filename:str)->TrainingData:
     """Parsing script for training data CSV written from ORCA C++ code. For
@@ -58,9 +78,9 @@ def read_training_data(filename:str)->TrainingData:
     with open(filename) as datafile:
         datareader = csv.reader(datafile, delimiter=',')
 
-        n_episodes = datareader.__next__()[0][0:-7] # remove the chars " robots"
-        n_agents   = datareader.__next__()[0][0:-9] # remove the chars " episodes"
-        dt         = datareader.__next__()[0][0:-8] # remove the chars " Delta-t"
+        n_episodes = datareader.__next__()[1] # Get the number of episodes
+        n_agents   = datareader.__next__()[1] # Get the number of agents
+        dt         = datareader.__next__()[1] # Get the simulation timestep
 
         param_dict["n_episodes"] = int(n_episodes)
         param_dict["n_agents"]   = int(n_agents)
@@ -73,11 +93,17 @@ def read_training_data(filename:str)->TrainingData:
         # data.traj[episode].Px[agent][time_index])
 
         for ep in range(data.n_episodes):
-            row = datareader.__next__()
-            for t in range(1,len(row)-1,data.n_agents):
+            header_row = datareader.__next__()
+            radius_row = datareader.__next__()
+            vmax_row   = datareader.__next__()
+            goal_row   = datareader.__next__()
+            traj_row   = datareader.__next__()
+
+            print(header_row)
+            for t in range(1,len(traj_row)-1,data.n_agents):
                 for ag in range(data.n_agents):
                     # strip new line character, remove parens, split at space
-                    r = row[t+ag].rstrip("\n").replace("(","").replace(")","").split(" ")
+                    r = traj_row[t+ag].rstrip("\n").replace("(","").replace(")","").split(" ")
 
                     # assign floats to traj struct
                     data.traj[ep].Px[ag] = np.append(data.traj[ep].Px[ag], float(r[1]))
@@ -92,4 +118,31 @@ def read_training_data(filename:str)->TrainingData:
                         data.traj[ep].X[ag]  = np.hstack((data.traj[ep].X[ag], \
                             np.array([float(r[1]), float(r[2]), float(r[3]), float(r[4])])))
 
+            for idx in range(data.n_agents):
+                data.traj[ep].R[idx]    = float(radius_row[idx+1])
+                data.traj[ep].Vmax[idx] = float(vmax_row[idx+1])
+
+                g = goal_row[idx+1].replace("(","").replace(")","").split(" ")
+
+                data.traj[ep].Pg[idx][0]   = float(g[1])
+                data.traj[ep].Pg[idx][1]   = float(g[2])
+
     return data
+
+
+## Use for testing/checking outputs
+# data_out = read_training_data(filename)
+
+# print("EP 1")
+# print(data_out.traj[0].Px[0][0:2])
+# print(data_out.traj[0].R)
+# print(data_out.traj[0].Pg[0])
+# print(data_out.traj[0].Pg[1])
+# print(data_out.traj[0].Vmax)
+
+# print("EP 2")
+# print(data_out.traj[1].Px[0][0:2])
+# print(data_out.traj[1].R)
+# print(data_out.traj[1].Pg[0])
+# print(data_out.traj[1].Pg[1])
+# print(data_out.traj[1].Vmax)
--- src/Generate_Training_Data.cpp
+++ src/Generate_Training_Data.cpp
@@ -53,10 +53,9 @@
 const float M_PI = 3.14159265358979323846f;
 #endif
 
-/* Store the goals of the agents. */
-std::vector<RVO::Vector2> goals;
-
-void setupScenario(RVO::RVOSimulator *sim, int n_agents, float dim_world, float dt)
+void setupScenario(RVO::RVOSimulator *sim, int n_agents, float dim_world, float dt, 
+					std::vector<float> radius, std::vector<float> vmax, 
+					std::vector<RVO::Vector2> goals)
 {
 	/* Specify the global time step of the simulation. */
 	sim->setTimeStep(dt);
@@ -76,18 +75,18 @@ void setupScenario(RVO::RVOSimulator *sim, int n_agents, float dim_world, float 
     float range_x =  dim_world;
     float range_y =  dim_world;
 
-    float x, y, xg, yg;
+    float x, y; //, xg, yg;
 
     for (int i = 0; i < n_agents; ++i){
 		/* Assign random states and random goals to the agents */
-        x = range_x * (((float)rand()/RAND_MAX) - 0.5); xg = range_x * (((float)rand()/RAND_MAX) - 0.5);
-        y = range_y * (((float)rand()/RAND_MAX) - 0.5); yg = range_y * (((float)rand()/RAND_MAX) - 0.5);
+        x = range_x * (((float)rand()/RAND_MAX) - 0.5); // xg = range_x * (((float)rand()/RAND_MAX) - 0.5);
+        y = range_y * (((float)rand()/RAND_MAX) - 0.5); // yg = range_y * (((float)rand()/RAND_MAX) - 0.5);
         sim->addAgent(RVO::Vector2(x, y));
-        goals.push_back(RVO::Vector2(xg, yg));
+        // goals.push_back(RVO::Vector2(xg, yg));
     }
 }
 
-void setPreferredVelocities(RVO::RVOSimulator *sim)
+void setPreferredVelocities(RVO::RVOSimulator *sim, std::vector<RVO::Vector2> goals)
 {
 	/*
 	 * Set the preferred velocity to be a vector of unit magnitude (speed) in the
@@ -116,7 +115,7 @@ void setPreferredVelocities(RVO::RVOSimulator *sim)
 	}
 }
 
-bool reachedGoal(RVO::RVOSimulator *sim, float thresh)
+bool reachedGoal(RVO::RVOSimulator *sim, float thresh, std::vector<RVO::Vector2> goals)
 {
 	/* Check if all agents have reached their goals. */
 
@@ -134,30 +133,85 @@ int main()
 	/* Set randomizer seed. Fix this number to produce repeatable data */
 	std::srand(static_cast<unsigned int>(std::time(NULL)));
 
+	int  episode 		= 0;			// intialize the episode counter
+
+	/* flags */
+	bool homogeneous_radius = 1;		// True: All robots have same radius, False: Randomize each robot's radius
+	bool homogeneous_vmax   = 1;		// True: All robots have same vmax  , False: Randomize each robot's vmax
+
 	/* Set Simulation Parameters*/
-	float dim_world 	= 10; 	// assumes square world with side length dim_world
-	float dt 			= 0.25;
-	int   episode 		= 0;
-	int   n_agents 		= 2;
-    int   N_episodes 	= 2;
+	float dim_world 	= 10; 			// assumes square world with side length dim_world
+	float dt 			= 0.25;			// simulation time step
+	int   n_agents 		= 2;			// number of agents to simulate
+    int   N_episodes 	= 2;			// number of simulated trajectories to generate
+	float rmax 			= dim_world/100;// maximum value for robot radius
+	float vmaxmax 		= dim_world/10;	// maximum value for vmax
 
 	/* Data File Row Organization:
 	Episode, (px1(t0) py1(t0)) (vx1(t0) vy1(t0)), ..., (pxn(t0) pyn(t0)) (vxn(t0) vyn(t0)),...,
 	  		 (px1(tf) py1(tf)) (vx1(tf) vy1(tf)), ..., (pxn(tf) pyn(tf)) (vxn(tf) vyn(tf))
 	Each episode maintains it's own row in the file. The number of robots, number of episodes, 
-	and simulation dt is written at the top of the file */
+	robot radii, robot vmaxs, and simulation dt is written at the top of the file */
 	std::ofstream training_data_file("data/training_data.csv");
 
 	/* Write header information */
-	training_data_file << n_agents << " Robots" << std::endl;
-	training_data_file << N_episodes << " Epsiodes" << std::endl;
-	training_data_file << dt << " Delta-t";
-
+	training_data_file << "Robots, " << n_agents << std::endl;
+	training_data_file << "Episodes, " << N_episodes << std::endl;
+	training_data_file << "T, " << dt << std::endl;
 
     while (episode < N_episodes){
 
-        std::cout << "----- Generating Episode: " << ++episode << " -----" << std::endl;
-		training_data_file << std::endl << episode <<", ";
+		training_data_file << "START " << ++episode << std::endl;
+
+		std::vector<float> radius = {rmax};
+		std::vector<float> vmax   = {vmaxmax};
+		std::vector<RVO::Vector2> goals;
+		float xg; float yg;
+		for (int i = 0; i < n_agents; i++){
+			/* generate radius for each robot */
+			if (homogeneous_radius == 1){
+				/* If homogeneous, use same radius for all agents */
+				radius.push_back(rmax);
+			} else {
+				/* Otherwise, assign a random radius with max of radius[0] */
+				radius.push_back(((float)rand()/RAND_MAX) * rmax);
+			}
+
+			/* generate vmax for each robot */
+			if (homogeneous_vmax == 1){
+				/* If homogeneous, use same vmax for all agents */
+				vmax.push_back(vmaxmax);
+			} else {
+				/* Otherwise, assign a random radius with max of vmaxmax */
+				vmax.push_back(((float)rand()/RAND_MAX) * vmaxmax);
+			}
+
+			/* randomize goal state for each robot */
+			xg = dim_world * (((float)rand()/RAND_MAX) - 0.5);
+			yg = dim_world * (((float)rand()/RAND_MAX) - 0.5);
+			goals.push_back(RVO::Vector2(xg, yg));
+		}
+
+		training_data_file << "Radius, ";
+		for (int i = 0; i < n_agents-1; i++){
+			training_data_file << radius[i] << ", ";
+		}
+		training_data_file << radius[n_agents-1] << std::endl;
+
+		training_data_file << "Vmax, ";
+		for (int i = 0; i < (n_agents-1); i++){
+			training_data_file << vmax[i] << ", ";
+		}
+		training_data_file << vmax[n_agents-1] << std::endl;
+		
+		training_data_file << "Goal, ";
+		for (int i = 0; i < (n_agents-1); i++){
+			training_data_file << "(" << goals[i].x() << " " << goals[i].y() << "), ";
+		}
+		training_data_file << "(" << goals[n_agents-1].x() << " " << goals[n_agents-1].y() << ")"<< std::endl;
+
+        std::cout << "----- Generating Episode: " << episode << " -----" << std::endl;
+		training_data_file << episode;
 
         /* Create a new simulator instance. */
         RVO::RVOSimulator *sim = new RVO::RVOSimulator();
@@ -167,7 +221,7 @@ int main()
 		RVO::Vector2 current_vel;
 
         /* Set up a randomized scenario. */
-        setupScenario(sim, n_agents, dim_world, dt);
+        setupScenario(sim, n_agents, dim_world, dt, radius, vmax, goals);
 
         do {
 
@@ -175,16 +229,19 @@ int main()
 			for (int p = 0; p < n_agents; p++){
 				current_pos = sim->getAgentPosition(p);
 				current_vel = sim->getAgentVelocity(p);
-				training_data_file << "("<< current_pos.x() << " " << current_pos.y() <<") (" << current_vel.x() << " " << current_vel.y() <<"), ";
+				training_data_file << ", ("<< current_pos.x() << " " << current_pos.y() <<") (" << current_vel.x() << " " << current_vel.y() <<")";
 			}
 
-            setPreferredVelocities(sim);
+            setPreferredVelocities(sim, goals);
             sim->doStep();
         }
 		/* Adjust goal threshold using the size of the world */
-        while (!reachedGoal(sim, dim_world/100));
+        while (!reachedGoal(sim, dim_world/100, goals));
+
+		training_data_file << std::endl;
 
         delete sim;
+		radius.clear(); vmax.clear(); goals.clear();
     }
 
 	/* Write data file */
