7f29fbac17260752c8a6bb3d8f1d8dda5b030f94
Brian Dobkowski <bdobkowski14@comcast.net>
2022-02-27 19:51:57 -0800

    adding CADRL algorithm in deep_v_network.py file. Changes to model.py for data processing

--- scripts/reinforcement_learning/model.py
+++ scripts/reinforcement_learning/model.py
@@ -15,11 +15,17 @@ import numpy as np
 from sklearn.neighbors import radius_neighbors_graph
 import tensorflow as tf
 from read_training_data import read_training_data
-from utils import JointState
+import os
+import json
+# from utils import JointState
 #import tf.keras as keras
 # Local Application Modules
 # -----------------------------------------------------------------------------
 
+USER = 'Brian'
+# USER = 'Torstein'
+# USER = 'Valentin'
+# USER = 'Bradley'
 
 def state_value_pair(s_jn, t_g, v_pref, gamma):
     y = gamma**(t_g*v_pref)
@@ -83,6 +89,34 @@ def get_joint_state(s_robo1, s_robo2, radius, pgx, pgy, v_pref):
     x[13] = radius #radius1
     return x
 
+def get_joint_state2(s_robo1, s_robo2):
+    dim = 14
+    x = np.zeros((dim))
+    x[:4] = s_robo1[:4]
+    x[4] = s_robo1[9] 
+    x[5] = s_robo1[5] 
+    x[6] = s_robo1[6] 
+    x[7] = s_robo1[7] 
+    x[8] = s_robo1[8] 
+    x[9:13] = s_robo2[:4]
+    x[13] = s_robo1[9] #radius1
+    return x
+
+def get_state(s_robo1, radius, pgx, pgy, v_pref):
+    dim = 10
+    vx = s_robo1[2]
+    vy = s_robo1[3]
+    theta = np.arctan2(vy,vx)
+    x = np.zeros((dim))
+    x[:4] = s_robo1[:4]
+    x[4] = radius
+    x[5] = pgx 
+    x[6] = pgy 
+    x[7] = v_pref
+    x[8] = theta
+    x[9] = radius #radius1
+    return x
+
 def get_rotated_state(x):
     Pg = x[5:7]
     P = x[:2]
@@ -111,11 +145,12 @@ def get_rotated_state(x):
     rot[14] = np.linalg.norm(x[:2]-x[9:11], 2)
     return rot
 
-def load_traj_generate_data(folder):
+def load_traj_generate_data_not_joint(folder):
  
     output_shape = (1,)
-    #x, y = generate_random_training_data(input_shape, output_shape, N)
-    data = read_training_data("/home/torstein/Stanford/aa277/aa277_project/data/training_data.csv")
+    # x, y = generate_random_training_data(input_shape, output_shape, N)
+    data = read_training_data(os.path.join(folder, 'training_data_2sim_example.csv'))
+    # data = read_training_data("/home/torstein/Stanford/aa277/aa277_project/data/training_data.csv")
     
     robo1 = 0
     robo2 = 1
@@ -127,12 +162,81 @@ def load_traj_generate_data(folder):
 
     gamma = 0.999
 
-    xs = []
-    ys = []
+    x_ep_dict = {}
+    y_ep_dict = {}
 
     #generating for robot1 first: 
     #after, we  can do the same for robot2
     for ep in range(episodes_count):
+
+        x_robot_dict = {}
+        y_robot_dict = {}
+
+        traj = data.traj[ep]
+        for robot in range(len(traj.Pg)):
+            pgx = traj.Pg[robot][0]
+            pgy = traj.Pg[robot][1]
+            Vmax = max(traj.Vmax[0], traj.Vmax[1])
+            v_pref = Vmax
+            N = len(data.traj[ep].X[robot])
+
+            xs = []
+            ys = []
+            for i in range(N):
+
+                s_robo1 = data.traj[ep].X[robot][i]
+                state = get_state(s_robo1, radius, pgx, pgy, v_pref)
+                
+                tg = i*dt
+                y = gamma**(tg*v_pref)
+                xs.append(state.tolist())
+                ys.append(y.tolist())
+
+            x_robot_dict[robot] = np.array(xs)
+            y_robot_dict[robot] = np.array(ys)
+
+        x_ep_dict[ep] = x_robot_dict
+        y_ep_dict[ep] = y_robot_dict
+
+    return x_ep_dict, y_ep_dict
+
+    # y_json = json.dumps(y_ep_dict)
+    # with open(os.path.join(folder, 'y_dict.json'),"w") as f:
+    #     f.write(y_json)
+
+    # x_json = json.dumps(x_ep_dict)
+    # with open(os.path.join(folder, 'x_dict.json'),"w") as f:
+    #     f.write(x_json)
+
+def load_traj_generate_data(folder):
+ 
+    output_shape = (1,)
+    # x, y = generate_random_training_data(input_shape, output_shape, N)
+    data = read_training_data(os.path.join(folder, 'training_data_2sim_example.csv'))
+    # data = read_training_data("/home/torstein/Stanford/aa277/aa277_project/data/training_data.csv")
+    
+    robo1 = 0
+    robo2 = 1
+
+    episodes_count = len(data.traj)
+    dt = data.dt
+
+    radius = 0.1
+
+    gamma = 0.999
+
+    x_dict = {}
+    y_dict = {}
+    x_dict_rotated = {}
+
+    #generating for robot1 first: 
+    #after, we  can do the same for robot2
+    for ep in range(episodes_count):
+
+        xs_rotated = []
+        xs = []
+        ys = []
+
         traj = data.traj[ep]
         pgx = traj.Pg[robo1][0]
         pgy = traj.Pg[robo1][1]
@@ -149,41 +253,82 @@ def load_traj_generate_data(folder):
             rotated_state = get_rotated_state(state)
             tg = i*dt
             y = gamma**(tg*v_pref)
-            xs.append(rotated_state)
-            ys.append(y)
+            xs_rotated.append(rotated_state.tolist())
+            xs.append(state.tolist())
+            ys.append(y.tolist())
 
+        x_dict_rotated[ep] = xs_rotated
+        x_dict[ep] = xs
+        y_dict[ep] = ys
 
-    xs, ys = np.array(xs), np.array(ys)
+    # all_xs, all_ys = np.array(xs), np.array(ys)
 
-    with open(f"{folder}/xs.npy", 'wb') as f:
-        np.save(f, xs)
-    with open(f"{folder}/ys.npy", 'wb') as f:
-        np.save(f, ys)
+    # with open(f"{folder}/xs.npy", 'wb') as f:
+    #     np.save(f, all_xs)
+    # with open(f"{folder}/ys.npy", 'wb') as f:
+    #     np.save(f, all_ys)
 
+    x_json = json.dumps(x_dict_rotated)
+    with open(os.path.join(folder, 'x_dict_rotated.json'),"w") as f:
+        f.write(x_json)
 
-def test_model(folder):
+    y_json = json.dumps(y_dict)
+    with open(os.path.join(folder, 'y_dict.json'),"w") as f:
+        f.write(y_json)
 
+    x_json = json.dumps(x_dict)
+    with open(os.path.join(folder, 'x_dict.json'),"w") as f:
+        f.write(x_json)
 
-    with open(f"{folder}/xs.npy", 'rb') as f:
-        xs = np.load(f, allow_pickle=True)
-    with open(f"{folder}/ys.npy", 'rb') as f:
-        ys = np.load(f, allow_pickle=True)
+def load_training_test_data(folder):
+    # with open(f"{folder}/xs.npy", 'rb') as f:
+    #     xs = np.load(f, allow_pickle=True)
+    # with open(f"{folder}/ys.npy", 'rb') as f:
+    #     ys = np.load(f, allow_pickle=True)
 
-    input_shape = (xs[0].shape[0], )
-    model = create_model(input_shape)
+    with open(os.path.join(folder, 'x_dict_rotated.json'), 'r') as j:
+        x_dict = json.loads(j.read())
+    with open(os.path.join(folder, 'y_dict.json'), 'r') as j:
+        y_dict = json.loads(j.read())
 
+    xs = np.array([]).reshape((0, len(x_dict['0'][0])))
+    ys = np.array([])
+
+    for xk, yk in zip(x_dict.keys(), y_dict.keys()):
+        xs = np.vstack([xs, np.asarray(x_dict[xk])])
+        ys = np.concatenate((ys, np.asarray(y_dict[yk])))
 
     split = 2*len(ys)//3
+
     x_train = xs[:split]
     y_train = ys[:split]
 
     x_test = xs[split:]
     y_test = ys[split:]
-    model = train_model(model, x_train, y_train, epochs=10)
+
+    return x_train, y_train, x_test, y_test
+
+
+def test_model(folder):
+
+    x_train, y_train, x_test, y_test = load_training_test_data(folder)
+    
+    input_shape = (x_train[0].shape[0], )
+    model = create_model(input_shape)
+    
+    model = train_model(model, x_train, y_train, epochs=1000)
     results = model.evaluate(x_test, y_test, batch_size=128)
     print(results)
-    
-folder = "/home/torstein/Stanford/aa277/aa277_project/data"
-#load_traj_generate_data(folder)
-test_model(folder=folder)
+
+    model.save(folder)
+
+if __name__ == '__main__':
+    if USER == 'Torstein':
+        folder = "/home/torstein/Stanford/aa277/aa277_project/data"
+    elif USER == 'Brian':
+        folder  = "/home/bdobkowski/Stanford/AA277/aa277_project/data"
+    else:
+        raise Exception('Need to list a folder in on your local machine to store data')
+    load_traj_generate_data(folder)
+    test_model(folder=folder)
 
